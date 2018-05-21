c2da612cb7563699503ec96ef24267eeb9fb0b8c
charleslien <charleslien97@gmail.com>
2018-05-21 16:01:09 -0700

    Finished building deck class

--- Deck.py
+++ Deck.py
@@ -1,3 +1,5 @@
+import random
+
 # This should represent the deck for dev cards. It should include the 
 # correct number of each dev card and should have this as a stack data
 # structure. There should be a function that allows a player to take 
@@ -6,21 +8,36 @@ class Deck():
 
     # TODO: complete the constructor
     def __init__(self):
-        self.stack = None
-
-
-
-    # TODO: make a class that represents different development cards.
-    # This could be written inside of the deck class or we could 
-    # put it in its own file. 
+        '''
+        0 - Knight
+        1 - Victory point
+        2 - Road building
+        3 - Monopoly
+        4 - Year of plenty
+        '''
+        self.cards_left = 25
+        self.stack = [0] * 14 + [1] * 5 + [2] * 2 + [3] * 2 + [4] * 2
+        random.shuffle(self.stack)
+    
+    def get_card_name(card):
+        '''
+        returns the name of the card id given
+        '''
+        return {0 : 'Knight', 1 : 'Victory Point',\
+            2 : 'Road Building', 3 : 'Monopoly', 4 : 'Year of Plenty'}[card]
 
 
     # TODO: write a function that initializes a stack of cards. 
     def initialize_stack(self):
-        return
+        self.cards_left = 25
+        self.stack = [0] * 14 + [1] * 5 + [2] * 2 + [3] * 2 + [4] * 2
+        random.shuffle(self.stack)
 
 
     # TODO: write a function that allows a player to pull a dev
     # card from the deck and updates the player's hand of dev cards.
     def take_card(self, player):
-        return
+        if cards_left > 0:
+            self.cards_left -= 1
+            return self.stack[self.cards_left]
+        return -1
